20d8cbcad5986044575ef6503a0503455b54772c
Faustine Dengu <faustine.dengu@july28.co.zw>
2026-04-13 15:48:48 +0200

    completed exercises 3.1-3.8: full functionality and morgan logging

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
