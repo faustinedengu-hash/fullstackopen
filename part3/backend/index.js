29f2501867d0997e7a87ce565e190f40f27897f9
Faustine Dengu <faustine.dengu@july28.co.zw>
2026-04-13 14:29:26 +0200

    exercise 3.3

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-// 1. Data comes first
 let persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
   { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
@@ -9,7 +8,6 @@ let persons = [
   { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
 ]
 
-// 2. Then your routes
 app.get('/', (request, response) => {
   response.send('<h1>Hello Phonebook!</h1>')
 })
@@ -26,8 +24,16 @@ app.get('/info', (request, response) => {
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
 
-// 3. The server setup stays at the bottom
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
