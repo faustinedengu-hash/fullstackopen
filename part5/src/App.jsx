d1e82a9ac661bb021392005e8f63bc7a575e5968
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-04-23 12:47:28 +0200

    feat: complete exercise 5.3, blog creation with url and author display

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,33 +1,41 @@
 import { useState, useEffect } from 'react'
+import Blog from './components/Blog' // The course expects this component
+import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
+  const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
 
-  // NEW: 1. Put the useEffect right here, immediately after your state variables
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  // Exercise 5.1 requirement: Fetch blogs from the server
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )  
+  }, [])
+
+  // Exercise 5.2 requirement: Check for logged in user in local storage
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
-    
     try {
-      const user = await loginService.login({
-        username, password,
-      })
-      
-      // NEW: 2. Save the user to Local Storage right before you set the state
-      window.localStorage.setItem(
-        'loggedBlogappUser', JSON.stringify(user)
-      ) 
-      
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user)) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -36,48 +44,56 @@ const App = () => {
     }
   }
 
-  // NEW: 3. Create the handleLogout function right below handleLogin
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogappUser')
     setUser(null)
   }
 
+  // Exercise 5.3 requirement: Handle creation of a new blog
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const newBlog = await blogService.create({ title, author, url })
+      setBlogs(blogs.concat(newBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (exception) {
+      console.log('Error creating blog')
+    }
+  }
+
+  // VIEW 1: Login Form
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
         <form onSubmit={handleLogin}>
-          <div>
-            username
-              <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-              <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
+          <div>username <input value={username} onChange={({ target }) => setUsername(target.value)} /></div>
+          <div>password <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} /></div>
           <button type="submit">login</button>
         </form>
       </div>
     )
   }
 
+  // VIEW 2: Blog list and Creation Form
   return (
     <div>
-      <h2>Blogs</h2>
-      {/* NEW: 4. Add the button right next to the user's name */}
-      <p>
-        {user.name} logged in <button onClick={handleLogout}>logout</button>
-      </p>
+      <h2>blogs</h2>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+
+      <h2>create new</h2>
+      <form onSubmit={handleCreateBlog}>
+        <div>title: <input value={title} onChange={({ target }) => setTitle(target.value)} /></div>
+        <div>author: <input value={author} onChange={({ target }) => setAuthor(target.value)} /></div>
+        <div>url: <input value={url} onChange={({ target }) => setUrl(target.value)} /></div>
+        <button type="submit">create</button>
+      </form>
+
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
     </div>
   )
 }
---
+++ part5/src/services/blogs.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+let token = null
+
+// This function updates our private token variable
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+// This sends the POST request with the Authorization header
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+export default { getAll, create, setToken }
