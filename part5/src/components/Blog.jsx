8b2d2a2257526216b2402544b0034e280e96809a
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-04-28 09:59:19 +0200

    feat: complete exercise 5.7, add toggleable view for blog details

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,15 +1,43 @@
-const Blog = ({ blog }) => (
-  <div style={{ padding: 10, border: 'solid', borderWidth: 1, marginBottom: 5 }}>
-    <div>
-      <strong>Title:</strong> {blog.title}
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  // CSS for the blog border and spacing
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} 
+        <button onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+
+      {/* This section only renders if visible is true */}
+      {visible && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} 
+            <button>like</button>
+          </div>
+          <div>{blog.user ? blog.user.name : 'unknown user'}</div>
+        </div>
+      )}
     </div>
-    <div>
-      <strong>Author:</strong> {blog.author}
-    </div>
-    <div>
-      <strong>URL:</strong> {blog.url}
-    </div>
-  </div>
-)
+  )
+}
 
 export default Blog
