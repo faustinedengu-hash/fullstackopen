d6949e6c4948dd3d5d6c1e62e52592b7b9169cc6
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-04-30 10:30:38 +0200

    completed 5.25: individual user view with dynamic routing

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,12 +1,33 @@
-import userService from './services/users'
-import { Routes, Route, Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import { Routes, Route, Link, useParams } from 'react-router-dom'
 import Blog from './components/Blog' 
 import blogService from './services/blogs'
 import loginService from './services/login'
+import userService from './services/users'
 import Notification from './components/Notification' 
 import BlogForm from './components/BlogForm'
 import Togglable from './components/Togglable'
+
+const User = ({ users }) => {
+  const id = useParams().id
+  const user = users.find(u => u.id === id)
+
+  if (!user) {
+    return <p>User not found</p>
+  }
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <h3>added blogs</h3>
+      <ul>
+        {user.blogs.map(blog => (
+          <li key={blog.id}>{blog.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -21,19 +42,11 @@ const App = () => {
     return loggedUserJSON ? JSON.parse(loggedUserJSON) : null
   })
 
-  // Load blogs on startup
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )  
+    blogService.getAll().then(blogs => setBlogs(blogs))   
+    userService.getAll().then(initialUsers => setUsers(initialUsers))
   }, [])
-  useEffect(() => {
-  userService.getAll().then(initialUsers => {
-    setUsers(initialUsers)
-  })
-}, [])
 
-  // Ensure token is set if user is logged in
   useEffect(() => {
     if (user) {
       blogService.setToken(user.token)
@@ -50,7 +63,6 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch (exception) {
-      // FIXED: Changed setErrorMessage to setNotificationMessage
       setNotificationType('error')
       setNotificationMessage('wrong username or password')
       setTimeout(() => { setNotificationMessage(null) }, 5000)
@@ -66,13 +78,10 @@ const App = () => {
     try {
       const returnedBlog = await blogService.create(blogObject)
       setBlogs(blogs.concat(returnedBlog))
-      
       setNotificationType('success')
       setNotificationMessage(`a new blog ${blogObject.title} by ${blogObject.author} added`)
       setTimeout(() => setNotificationMessage(null), 5000)
     } catch (error) {
-      // Added console.error so you can see if something else breaks
-      console.error('Add blog failed:', error)
       setNotificationType('error')
       setNotificationMessage('error adding blog')
       setTimeout(() => setNotificationMessage(null), 5000)
@@ -95,24 +104,6 @@ const App = () => {
     }
   }
 
-  const handleLike = async (blog) => {
-    const updatedBlog = {
-      ...blog,
-      likes: (blog.likes || 0) + 1,
-      // Robust check for the user ID
-      user: blog.user.id || blog.user 
-    }
-
-    try {
-      const returnedBlog = await blogService.update(blog.id, updatedBlog)
-      setBlogs(blogs.map(b => b.id !== blog.id ? b : returnedBlog))
-    } catch (error) {
-      setNotificationType('error')
-      setNotificationMessage('error: could not update likes')
-      setTimeout(() => setNotificationMessage(null), 5000)
-    }
-  }
-
   if (user === null) {
     return (
       <div>
@@ -127,89 +118,54 @@ const App = () => {
     )
   }
 
- return (
+  return (
     <div>
       <nav style={{ background: '#eee', padding: '10px', marginBottom: '10px' }}>
-      <Link style={{ padding: '5px' }} to="/">blogs</Link>
-      <Link style={{ padding: '5px' }} to="/users">users</Link>
-    </nav>
+        <Link style={{ padding: '5px' }} to="/">blogs</Link>
+        <Link style={{ padding: '5px' }} to="/users">users</Link>
+      </nav>
 
-    <h2>blogs</h2>
-      <h2>blogs</h2>
+      <h2>blog app</h2>
       <Notification message={notificationMessage} type={notificationType} />
 
-      {user === null ? (
-        <form onSubmit={handleLogin}>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+
+      <Routes>
+        <Route path="/" element={
           <div>
-            username
-            <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
+            <Togglable buttonLabel='new blog'>
+              <BlogForm createBlog={addBlog} />
+            </Togglable>
+            {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
+              <Blog key={blog.id} blog={blog} handleDelete={() => handleDelete(blog)} user={user} />
+            )}
           </div>
+        } />
+
+        <Route path="/users" element={
           <div>
-            password
-            <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
+            <h2>Users</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th><strong>blogs created</strong></th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map(u => (
+                  <tr key={u.id}>
+                    <td><Link to={`/users/${u.id}`}>{u.name}</Link></td>
+                    <td>{u.blogs.length}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-          <button type="submit">login</button>
-        </form>
-      ) : (
-        <div>
-          <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+        } />
 
-          <Routes>
-            {/* The Home View: Show the form and the list of blogs */}
-            <Route path="/" element={
-              <div>
-                <Togglable buttonLabel='new blog'>
-                  <BlogForm createBlog={addBlog} />
-                </Togglable>
-
-                {blogs
-                  .sort((a, b) => b.likes - a.likes)
-                  .map(blog =>
-                    <Blog 
-                      key={blog.id} 
-                      blog={blog} 
-                      handleDelete={() => handleDelete(blog)} 
-                      user={user} 
-                    />
-                  )
-                }
-              </div>
-            } />
-
-            {/* Exercise 5.24 starts here: We will add the /users route next */}
-            <Route path="/users" element={
-              <div>
-                <h2>Users</h2>
-              <table>
-      <thead>
-        <tr>
-          <th></th>
-          <th><strong>blogs created</strong></th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map(u => (
-          <tr key={u.id}>
-            <td>{u.name}</td>
-            <td>{u.blogs.length}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-    </div>} />
-          </Routes>
-        </div>
-      )}
+        <Route path="/users/:id" element={<User users={users} />} />
+      </Routes>
     </div>
   )
 }
