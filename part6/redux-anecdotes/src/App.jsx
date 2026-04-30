8f9183c1081d6b3a841378dbbb1fe39d45e22900
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-04-30 16:31:57 +0200

    completed exercises 6.3 and 6.4: anecdotes voting and creation

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,47 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      payload: { content } // We only need to send the text now!
+    })
+  }
+
+  const vote = (id) => {
+    dispatch({
+      type: 'VOTE',
+      payload: { id }
+    })
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      {/* 1. Link the function to the form submit event */}
+      <form onSubmit={addAnecdote}>
+        {/* 2. Add the name "anecdote" so your function can find the value */}
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+export default App
