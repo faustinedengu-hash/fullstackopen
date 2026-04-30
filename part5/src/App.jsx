0ea302555edb203a93f72bbaf7be867db887d760
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-04-30 11:58:29 +0200

    finished 5.28: complete app styling with Material UI

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react'
 import { Routes, Route, Link, useParams } from 'react-router-dom'
+import { 
+  Container, TextField, Button, AppBar, Toolbar, 
+  IconButton, Table, TableBody, TableCell, TableContainer, 
+  TableRow, Paper, TableHead 
+} from '@mui/material'
+
 import Blog from './components/Blog' 
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -11,25 +17,19 @@ import Togglable from './components/Togglable'
 const User = ({ users }) => {
   const id = useParams().id
   const user = users.find(u => u.id === id)
-
-  if (!user) {
-    return <p>User not found</p>
-  }
+  if (!user) return <p>User not found</p>
 
   return (
     <div>
       <h2>{user.name}</h2>
       <h3>added blogs</h3>
       <ul>
-        {user.blogs.map(blog => (
-          <li key={blog.id}>{blog.title}</li>
-        ))}
+        {user.blogs.map(blog => <li key={blog.id}>{blog.title}</li>)}
       </ul>
     </div>
   )
 }
 
-// UPDATE: BlogView now has the comments form and list
 const BlogView = ({ blogs, handleLike, handleComment }) => {
   const [comment, setComment] = useState('')
   const id = useParams().id
@@ -49,22 +49,21 @@ const BlogView = ({ blogs, handleLike, handleComment }) => {
       <div><a href={blog.url}>{blog.url}</a></div>
       <div>
         {blog.likes} likes 
-        <button onClick={() => handleLike(blog)}>like</button>
+        <Button size="small" variant="outlined" onClick={() => handleLike(blog)}>like</Button>
       </div>
       <div>added by {blog.user ? blog.user.name : 'unknown'}</div>
 
       <h3>comments</h3>
       <form onSubmit={onSubmitComment}>
-        <input 
+        <TextField 
+          size="small" 
           value={comment} 
           onChange={({ target }) => setComment(target.value)} 
         />
-        <button type="submit">add comment</button>
+        <Button variant="contained" type="submit" style={{ marginLeft: 5 }}>add comment</Button>
       </form>
       <ul>
-        {(blog.comments || []).map((c, index) => (
-          <li key={index}>{c}</li>
-        ))}
+        {(blog.comments || []).map((c, index) => <li key={index}>{c}</li>)}
       </ul>
     </div>
   )
@@ -77,7 +76,6 @@ const App = () => {
   const [password, setPassword] = useState('') 
   const [notificationMessage, setNotificationMessage] = useState(null)
   const [notificationType, setNotificationType] = useState('success')
-
   const [user, setUser] = useState(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
     return loggedUserJSON ? JSON.parse(loggedUserJSON) : null
@@ -89,9 +87,7 @@ const App = () => {
   }, [])
 
   useEffect(() => {
-    if (user) {
-      blogService.setToken(user.token)
-    }
+    if (user) blogService.setToken(user.token)
   }, [user])
 
   const handleLogin = async (event) => {
@@ -106,7 +102,7 @@ const App = () => {
     } catch (exception) {
       setNotificationType('error')
       setNotificationMessage('wrong username or password')
-      setTimeout(() => { setNotificationMessage(null) }, 5000)
+      setTimeout(() => setNotificationMessage(null), 5000)
     }
   }
 
@@ -134,9 +130,6 @@ const App = () => {
       try {
         await blogService.remove(blog.id)
         setBlogs(blogs.filter(b => b.id !== blog.id))
-        setNotificationType('success')
-        setNotificationMessage(`Deleted ${blog.title}`)
-        setTimeout(() => setNotificationMessage(null), 5000)
       } catch (error) {
         setNotificationType('error')
         setNotificationMessage('error: could not delete blog')
@@ -146,12 +139,7 @@ const App = () => {
   }
 
   const handleLike = async (blog) => {
-    const updatedBlog = {
-      ...blog,
-      likes: (blog.likes || 0) + 1,
-      user: blog.user.id || blog.user 
-    }
-
+    const updatedBlog = { ...blog, likes: (blog.likes || 0) + 1, user: blog.user.id || blog.user }
     try {
       const returnedBlog = await blogService.update(blog.id, updatedBlog)
       setBlogs(blogs.map(b => b.id !== blog.id ? b : returnedBlog))
@@ -162,14 +150,10 @@ const App = () => {
     }
   }
 
-  // UPDATE: Added handleComment function here
   const handleComment = async (id, comment) => {
     try {
       const updatedBlog = await blogService.addComment(id, comment)
       setBlogs(blogs.map(b => b.id !== id ? b : updatedBlog))
-      setNotificationType('success')
-      setNotificationMessage('Comment added!')
-      setTimeout(() => setNotificationMessage(null), 5000)
     } catch (error) {
       setNotificationType('error')
       setNotificationMessage('error: could not add comment')
@@ -179,29 +163,33 @@ const App = () => {
 
   if (user === null) {
     return (
-      <div>
+      <Container>
         <h2>Log in to application</h2>
         <Notification message={notificationMessage} type={notificationType} />
         <form onSubmit={handleLogin}>
-          <div>username <input name="Username" value={username} onChange={({ target }) => setUsername(target.value)} /></div>
-          <div>password <input name="Password" type="password" value={password} onChange={({ target }) => setPassword(target.value)} /></div>
-          <button type="submit">login</button>
+          <div><TextField label="username" value={username} onChange={({ target }) => setUsername(target.value)} margin="normal" /></div>
+          <div><TextField label="password" type="password" value={password} onChange={({ target }) => setPassword(target.value)} margin="normal" /></div>
+          <Button variant="contained" color="primary" type="submit">login</Button>
         </form>
-      </div>
+      </Container>
     )
   }
 
   return (
-    <div>
-      <nav style={{ background: '#eee', padding: '10px', marginBottom: '10px' }}>
-        <Link style={{ padding: '5px' }} to="/">blogs</Link>
-        <Link style={{ padding: '5px' }} to="/users">users</Link>
-      </nav>
+    <Container>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/">blogs</Button>
+          <Button color="inherit" component={Link} to="/users">users</Button>
+          <div style={{ marginLeft: 'auto' }}>
+            <em>{user.name} logged in</em>
+            <Button color="inherit" onClick={handleLogout}>logout</Button>
+          </div>
+        </Toolbar>
+      </AppBar>
 
       <h2>blog app</h2>
       <Notification message={notificationMessage} type={notificationType} />
-
-      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
 
       <Routes>
         <Route path="/" element={
@@ -218,31 +206,31 @@ const App = () => {
         <Route path="/users" element={
           <div>
             <h2>Users</h2>
-            <table>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th><strong>blogs created</strong></th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map(u => (
-                  <tr key={u.id}>
-                    <td><Link to={`/users/${u.id}`}>{u.name}</Link></td>
-                    <td>{u.blogs.length}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell></TableCell>
+                    <TableCell><strong>blogs created</strong></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {users.map(u => (
+                    <TableRow key={u.id}>
+                      <TableCell><Link to={`/users/${u.id}`}>{u.name}</Link></TableCell>
+                      <TableCell>{u.blogs.length}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </div>
         } />
 
         <Route path="/users/:id" element={<User users={users} />} />
-        
-        {/* UPDATE: Passed handleComment to the BlogView route */}
         <Route path="/blogs/:id" element={<BlogView blogs={blogs} handleLike={handleLike} handleComment={handleComment} />} />
       </Routes>
-    </div>
+    </Container>
   )
 }
 
