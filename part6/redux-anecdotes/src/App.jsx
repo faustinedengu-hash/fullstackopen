d7a932ea13651f018afa9a1e6068c9cd07d01ffc
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-04 11:37:37 +0200

    completed exercises 6.7 and 6.8: extracted components

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,47 +1,12 @@
-
-import { useSelector, useDispatch } from 'react-redux'
-// 1. Import the action creators (You had this perfect!)
-import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => 
-    [...state].sort((a, b) => b.votes - a.votes)
-  )
-  const dispatch = useDispatch()
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    
-    // 2. USE the action creator here instead of the raw object!
-    dispatch(createAnecdote(content)) 
-  }
-
-  const vote = (id) => {
-    // 3. USE the action creator here instead of the raw object!
-    dispatch(voteAnecdote(id))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote" /></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
