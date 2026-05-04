9c7a93e2771a218c65bc9fd898e87b1c3db0d863
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-04 11:44:49 +0200

    completed exercise 6.9: implemented filter and combineReducers

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,9 +2,17 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => 
-    [...state].sort((a, b) => b.votes - a.votes)
-  )
+  // 1. The new filtering useSelector
+  const anecdotes = useSelector(state => {
+    if (state.filter === '') {
+      return [...state.anecdotes].sort((a, b) => b.votes - a.votes)
+    }
+    return state.anecdotes
+      .filter(a => a.content.toLowerCase().includes(state.filter.toLowerCase()))
+      .sort((a, b) => b.votes - a.votes)
+  })
+
+  // 2. We still need dispatch for the vote button!
   const dispatch = useDispatch()
 
   const vote = (id) => {
--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,8 +1,15 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+
+// This creates a state object that looks like: { anecdotes: [...], filter: '' }
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  filter: filterReducer
+})
 
 const store = createStore(reducer)
 
