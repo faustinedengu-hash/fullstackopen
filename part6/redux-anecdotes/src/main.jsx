cf3d357d0a321b00d346738695cdf34d64024bd5
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-04 11:50:18 +0200

    completed exercise 6.10: installed Redux Toolkit and refactored filter to a slice

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,17 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
-
-// This creates a state object that looks like: { anecdotes: [...], filter: '' }
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer)
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
