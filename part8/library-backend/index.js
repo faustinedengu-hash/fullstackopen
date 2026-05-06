6ed3b4c40fa9e04f8f1f84ae32be5497e3aef1c4
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-06 11:16:46 +0200

    completed exercises 8.1-8.5: added allAuthors bookCount field

---
+++ part8/library-backend/index.js
@@ -0,0 +1,65 @@
+const { ApolloServer } = require('@apollo/server')
+const { startStandaloneServer } = require('@apollo/server/standalone')
+
+let authors = [
+  { name: 'Robert Martin', id: "afa51ab0-344d-11e9-a414-719c6709cf3e", born: 1952 },
+  { name: 'Martin Fowler', id: "afa51bb0-344d-11e9-a414-719c6709cf3e", born: 1963 },
+  { name: 'Fyodor Dostoevsky', id: "afa51cc0-344d-11e9-a414-719c6709cf3e", born: 1821 },
+  { name: 'Joshua Kerievsky', id: "afa51de0-344d-11e9-a414-719c6709cf3e" },
+  { name: 'Sandi Metz', id: "afa51ff0-344d-11e9-a414-719c6709cf3e" },
+]
+
+let books = [
+  { title: 'Clean Code', published: 2008, author: 'Robert Martin', id: "afa5b6f0-344d-11e9-a414-719c6709cf3e", genres: ['refactoring'] },
+  { title: 'Agile software development', published: 2002, author: 'Robert Martin', id: "afa5b6f1-344d-11e9-a414-719c6709cf3e", genres: ['agile', 'patterns', 'design'] },
+  { title: 'Refactoring, edition 2', published: 2018, author: 'Martin Fowler', id: "afa5de00-344d-11e9-a414-719c6709cf3e", genres: ['refactoring'] },
+  { title: 'Refactoring to patterns', published: 2004, author: 'Joshua Kerievsky', id: "afa5de01-344d-11e9-a414-719c6709cf3e", genres: ['refactoring', 'patterns'] },  
+  { title: 'Practical Object-Oriented Design in Ruby', published: 2012, author: 'Sandi Metz', id: "afa5de02-344d-11e9-a414-719c6709cf3e", genres: ['design', 'patterns'] },
+  { title: 'Crime and Punishment', published: 1866, author: 'Fyodor Dostoevsky', id: "afa5de03-344d-11e9-a414-719c6709cf3e", genres: ['classic', 'crime'] },
+  { title: 'The Idiot', published: 1869, author: 'Fyodor Dostoevsky', id: "afa5de04-344d-11e9-a414-719c6709cf3e", genres: ['classic', 'fantasy'] },
+]
+
+// typeDefs: Defines WHAT data can be fetched (The Schema)
+const typeDefs = `
+  type Author {
+    name: String!
+    born: Int
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
+  }
+`
+
+// resolvers: Defines HOW to get that data
+const resolvers = {
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: () => books,
+    allAuthors: () => authors
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
