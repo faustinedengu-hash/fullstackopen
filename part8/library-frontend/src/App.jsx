16866c2c6e3fc2bbc0a9a45c8386512742175edb
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-07 09:39:35 +0200

    8.10: Added new book mutation. 8.11: Added edit birthyear mutation.

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -1,87 +1,25 @@
-import { gql } from '@apollo/client'
-import { useQuery } from '@apollo/client/react'
-
-const ALL_AUTHORS = gql`
-  query {
-    allAuthors {
-      name
-      born
-      bookCount
-    }
-  }
-`
-
-// 1. We add the exact query you just tested
-const ALL_BOOKS = gql`
-  query {
-    allBooks {
-      title
-      author
-      published
-    }
-  }
-`
+import { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
 
 const App = () => {
-  // 2. We run both queries, giving them distinct variable names
-  const authorsResult = useQuery(ALL_AUTHORS)
-  const booksResult = useQuery(ALL_BOOKS)
-
-  // 3. Wait for both to finish loading
-  if (authorsResult.loading || booksResult.loading) {
-    return <div>loading...</div>
-  }
-
-  // 4. Catch errors for either query
-  if (authorsResult.error) {
-    return <div>Error loading authors: {authorsResult.error.message}</div>
-  }
-  if (booksResult.error) {
-    return <div>Error loading books: {booksResult.error.message}</div>
-  }
-
-  // 5. Extract the data
-  const authors = authorsResult.data.allAuthors
-  const books = booksResult.data.allBooks
+  const [page, setPage] = useState('authors')
 
   return (
     <div>
-      <h2>Authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th style={{ textAlign: 'left' }}>Name</th>
-            <th style={{ textAlign: 'left' }}>Born</th>
-            <th style={{ textAlign: 'left' }}>Books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
 
-      {/* 6. Add the new Books table right below it */}
-      <h2>Books</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th style={{ textAlign: 'left' }}>Title</th>
-            <th style={{ textAlign: 'left' }}>Author</th>
-            <th style={{ textAlign: 'left' }}>Published</th>
-          </tr>
-          {books.map((b) => (
-            <tr key={b.title}>
-              <td>{b.title}</td>
-              <td>{b.author}</td>
-              <td>{b.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Authors show={page === 'authors'} />
+      <Books show={page === 'books'} />
+      <NewBook show={page === 'add'} />
+      
+      {/* We'll build NewBook next! */}
+      {page === 'add' && <div>NewBook form will go here soon</div>}
     </div>
   )
 }
---
+++ part8/library-frontend/src/components/Authors.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/client/react'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+
+const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const result = useQuery(ALL_AUTHORS)
+  
+  // The mutation hook
+  const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
+
+  if (!props.show) return null
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const authors = result.data.allAuthors
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    editAuthor({ variables: { name, setBornTo: parseInt(born) } })
+
+    setName('')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
