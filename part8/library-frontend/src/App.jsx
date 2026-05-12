aea2e9d48eae56f85773758de44337827a72a183
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-12 11:52:15 +0200

    complete exercise 8.24: frontend subscriptions setup

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -1,15 +1,26 @@
+
 import { useState, useEffect } from 'react'
-import { useApolloClient } from '@apollo/client/react'
+import { useApolloClient, useSubscription } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import Recommend from './components/Recommend'
+import { BOOK_ADDED } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
   const client = useApolloClient()
+
+  // --- THE NEW SUBSCRIPTION HOOK ---
+  useSubscription(BOOK_ADDED, {
+    onData: ({ data }) => {
+      console.log('Subscription data received:', data)
+      const addedBook = data.data.bookAdded
+      window.alert(`A new book just dropped: ${addedBook.title} by ${addedBook.author.name}`)
+    }
+  })
 
   // Check if user is already logged in when the app starts
   useEffect(() => {
@@ -32,12 +43,11 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         
-        
         {token ? (
           <>
             <button onClick={() => setPage('add')}>add book</button>
+            <button onClick={() => setPage('recommend')}>recommend</button>
             <button onClick={logout}>logout</button>
-             <button onClick={() => setPage('recommend')}>recommend</button>
           </>
         ) : (
           <button onClick={() => setPage('login')}>login</button>
