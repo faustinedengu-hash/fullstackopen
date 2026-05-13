d55233a84e44e33ec43bc8c70a80eddba5787b25
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-13 11:13:49 +0200

    complete exercise 9.10: create diagnoses endpoint

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './src/routes/diagnoses'; // <-- Add this import
 const app = express();
 app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -11,6 +12,7 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+app.use('/api/diagnoses', diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
