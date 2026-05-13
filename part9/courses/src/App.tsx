461a50f52c564bc6229c9bb69f0bf8d2669e2b88
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-13 12:31:58 +0200

    complete exercise 9.14: typescript course info app

---
+++ part9/courses/src/App.tsx
@@ -0,0 +1,57 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+const Header = ({ name }: { name: string }) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }: { parts: CoursePart[] }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }: { parts: CoursePart[] }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  );
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts: CoursePart[] = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total parts={courseParts} />
+    </div>
+  );
+};
+
+export default App;
