56b5384e6b49ae1a921f0b8f438aa536d0501fdf
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-14 16:23:12 +0200

    part 10: add repository list (exercise 10.3)

---
+++ part10/rate-repository-app/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View } from 'react-native';
+import RepositoryList from './RepositoryList';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexGrow: 1,
+  },
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <RepositoryList />
+    </View>
+  );
+};
+
+export default Main;
---
+++ part10/rate-repository-app/src/components/RepositoryList.jsx
@@ -0,0 +1,51 @@
+
+import { FlatList, View, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+});
+
+const repositories = [
+  {
+    id: 'jaredpalmer.formik',
+    fullName: 'jaredpalmer/formik',
+    description: 'Build forms in React, without the tears',
+    language: 'TypeScript',
+    forksCount: 1589,
+    stargazersCount: 21553,
+    ratingAverage: 88,
+    reviewCount: 4,
+    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060100?v=4',
+  },
+  {
+    id: 'rails.rails',
+    fullName: 'rails/rails',
+    description: 'Ruby on Rails',
+    language: 'Ruby',
+    forksCount: 18349,
+    stargazersCount: 45377,
+    ratingAverage: 100,
+    reviewCount: 2,
+    ownerAvatarUrl: 'https://avatars1.githubusercontent.com/u/4223?v=4',
+  },
+];
+
+const ItemSeparator = () => <View style={styles.separator} />;
+
+const RepositoryList = () => {
+  return (
+    <FlatList
+      data={repositories}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.fullName}</Text>
+        </View>
+      )}
+    />
+  );
+};
+
+export default RepositoryList;
