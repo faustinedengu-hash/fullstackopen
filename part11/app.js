c605e623bf97b9ae2db6a2e552749c38a1ad87ac
faustinedengu-hash <faustine.dengu@july28.co.zw>
2026-05-20 11:39:14 +0200

    test: complete ex 11.12 - intentionally break health check to test rollback

--- part11/app.js
+++ part11/app.js
@@ -8,7 +8,8 @@ app.use(express.static('dist'))
 
 // 2026 UNIVERSITY STANDARD: Health check endpoint for zero-downtime orchestration
 app.get('/health', (req, res) => {
-  res.send('ok')
+  // EXERCISE 11.12: INTENTIONAL SABOTAGE TO TEST ROLLBACK
+  res.status(500).send('error')
 })
 
 app.listen(PORT, () => {
